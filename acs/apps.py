6a0e90ae34e640f5d50fea6f9bd6bbe8b9316616
iam-bkpl <kapil.bhandari@apexcollege.edu.np>
2023-04-26 22:58:49 +0545

    setup django

---
+++ acs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AcsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'acs'
---
+++ ashop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ashop'
